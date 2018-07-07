8b0e84bc8839d0d802ddd6d5f41d4a1ea6810583
NicolasCamigliano <40808680+NicolasCamigliano@users.noreply.github.com>
2018-07-06 21:56:45 -0300

    Ejercicios
    Del 6 al 10 de practica. del 1 al 3 sobre parcial (recu).

---
+++ jsEntradaSalida-7.js
@@ -0,0 +1,35 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostrar el resulto por medio de "ALERT"
+ej.: "la Resta es 750"*/ 
+function sumar()
+{	
+	
+}
+
+function restar()
+{
+var numeroUno; 
+var numeroDos;
+var resultado;
+
+numeroUno=document.getElementById('numeroUno').value;
+numeroUno=parseInt(numeroUno);
+numeroDos=document.getElementById('numeroDos').value;
+numeroDos=parseInt(numeroDos);
+resultado= numeroUno-numeroDos;
+
+alert("El resto es " + resultado);
+
+}
+
+function multiplicar()
+{ 
+	
+}
+
+function dividir()
+{
+	
+}
+
