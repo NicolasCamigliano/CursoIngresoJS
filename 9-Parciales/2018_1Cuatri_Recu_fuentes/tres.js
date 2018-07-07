13dd616ab10cea301c7c0621ada9753a85b0b961
NicolasCamigliano <40808680+NicolasCamigliano@users.noreply.github.com>
2018-07-06 21:58:54 -0300

    Ejercicios de Parcial
    Van del 1 al 3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,15 @@
 function mostrar()
 {
+var elPrecioFinal
+var dinero
+var descuento
+var iva
+var resultado
+
+elPrecioFinal=prompt("Ingresar precio y porcentaje descuento");
+	dinero=(dinero*1.10)
+descuento=(dinero*1.10)
+iva=(dinero*1.21)
+alert("El descuento en dinero es " + dinero + " El precio con el descuento es " + descuento + " El IVA es " + iva )
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+var base;
+var altura;
+var superficie;
+var perimetro;
+
+base=prompt("Ingrese base");
+base=parseInt(base)
+altura=prompt("Ingrese altura");
+altura=parseInt(altura)
+superficie= base*altura/2
+perimetro=base*3
+alert("La superficie es " + superficie + " y el perimetro es " + perimetro);
 
 }
