26c47d4747a81d8359dcf6b669d7c7f5378ccc1e
NicolasCamigliano <40808680+NicolasCamigliano@users.noreply.github.com>
2018-07-13 20:21:48 -0300

    Ejercicios Switch
    Clase 3. IFSwitch. Del 1 al 6.

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,7 +3,46 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+switch(mesDelAño)
+{case "Enero":
+	alert("Que comiences bien el año!!!.");
+	break;
+
+case "Marzo":
+	alert("a clases!!!.");
+	break;
+
+case "Julio":
+	alert("Se vienen las vacaciones!!!.");
+	break
+
+case "Diciembre":
+	alert("Felices fiesta!!!.");
+	break;}
+
+
+
 
 
 
 }//FIN DE LA FUNCIÓN
+	/* Ferreti iluminacion
+	if (DIA == "LUN" )
+	if (DIA == "MAR" )
+	if (DIA == "MIE" )
+	SWITCH
+
+	switch (dia) misma variable.
+		CASE "LUN":
+		alert ("lunes")
+		Break;
+		CASE "MAR":
+		alert ("lunes")
+		Break;
+		CASE "MIE":
+		alert ("lunes")
+		Break;
+
+
+
+*/
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,9 +4,21 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
-	
-
-
+switch(mesDelAño)
+{case "Febrero":
+	alert("Tiene más de 28 días.");
+	break;
+case "Abril":
+	alert("Tiene más de 30 días.");
+case "Junio":
+	alert("Tiene más de 30 días.");
+case "Septiembre":
+	alert("Tiene más de 30 días.");
+case "Noviembre":
+	alert("Tiene más de 30 días.");
+	break;
+default:
+alert("Tiene 31 dias");
+}
 
 }//FIN DE LA FUNCIÓN
